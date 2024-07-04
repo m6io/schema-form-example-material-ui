a1726d853d75564b8b1edf912decff825ca3c782
m6io <m5dk4r@gmail.com>
2024-07-04 13:50:23 -0400

    feat: app allows schema injection if encapsulated as an iframe

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,11 @@ import { JSONSchema7, FormProvider } from "@react-formgen/json-schema";
 import { Layout } from "./components/site/Layout";
 import { MuiFormComponent, muiCustomFields } from "./components/templates";
 import { ThemeProviderWithState } from "./components/site/ThemeContext";
+import isEqual from "lodash-es/isEqual";
 
 const App: React.FC = () => {
   const [schema, setSchema] = useState<JSONSchema7 | null>(null);
-
-  useEffect(() => {
-    const fetchSchema = async () => {
-      const response = await fetch("/schema.json");
-      const schema = await response.json();
-      setSchema(schema);
-    };
-
-    fetchSchema();
-  }, []);
-
-  const initialData = {
+  const [initialData, setInitialData] = useState<object>({
     firstName: "John Doe",
     lastName: "Doe",
     age: 30,
@@ -30,16 +20,68 @@ const App: React.FC = () => {
       city: "Somewhere",
       state: "CA",
     },
-  };
+  });
+  const [schemaKey, setSchemaKey] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchSchema = async () => {
+      const response = await fetch("/schema.json");
+      const schema = await response.json();
+      setSchema(schema);
+    };
+
+    const handleMessage = (event: MessageEvent) => {
+      if (event.origin === window.location.origin) {
+        const newSchema = event.data as JSONSchema7;
+        // If it's the first time or the schema is different, update it
+        if (!schema && schemaKey === 0) {
+          // Uncomment for debugging
+          // console.log("Schema is null, updating...");
+          setSchema(newSchema as JSONSchema7);
+          setSchemaKey((prevKey) => prevKey + 1); // Update the key to force re-render
+        }
+
+        if (schema && !isEqual(newSchema, schema)) {
+          // Uncomment for debugging
+          // console.log("Schema is not the same, updating...");
+          setSchema(newSchema as JSONSchema7);
+          setInitialData({});
+          setSchemaKey((prevKey) => prevKey + 1); // Update the key to force re-render
+        }
+
+        // else { // Uncomment for debugging
+        //   console.log("Schema is the same, ignoring...");
+        // }
+      }
+    };
+
+    if (window.self !== window.top) {
+      // Inside an iframe
+      window.addEventListener("message", handleMessage);
+
+      return () => {
+        window.removeEventListener("message", handleMessage);
+      };
+    } else {
+      // Standalone app
+      fetchSchema();
+    }
+  }, [schema, schemaKey]);
 
   return (
     <ThemeProviderWithState>
       <Layout>
         {schema && (
-          <FormProvider schema={schema} initialData={initialData}>
+          <FormProvider
+            key={schemaKey}
+            schema={schema}
+            initialData={initialData}
+          >
             <MuiFormComponent
               onSubmit={(data) => console.log("Form submitted:", data)}
-              onError={(errors) => console.error("Form errors:", errors)}
+              onError={(errors, data) =>
+                console.error("Form errors:", errors, data)
+              }
               customFields={muiCustomFields}
             />
           </FormProvider>
